394e8258bd270999f51ad42d9ca514aec3c98a8e
Ilja Leijo <ilja.leijo@gmail.com>
2016-06-06 00:08:33 +0300

    kickstarted a new angular project with yeoman generator

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mdCodingTestApp
+ * @description
+ * # mdCodingTestApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mdCodingTestApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mdCodingTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mdCodingTestApp
+ */
+angular.module('mdCodingTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
