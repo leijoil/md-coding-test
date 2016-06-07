2bce7a11edd350001aa1ec80f139672c8996210e
Ilja Leijo <ilja.leijo@gmail.com>
2016-06-08 00:56:31 +0300

    added e2e tests

--- test/e2e/chat.spec.js
+++ test/e2e/chat.spec.js
@@ -1,4 +1,4 @@
-describe('directive tests', function () {
+describe('e2e test for chat client', function () {
   var waitUntilReady = function (elm) {
     browser.wait(function () {
       return elm.isPresent()
@@ -14,8 +14,49 @@ describe('directive tests', function () {
     browser.waitForAngular()
   })
 
+  it('should connect with a nickname, send a message and disconnect', function () {
+    waitUntilReady(element(by.model('chat.nick.name')))
+    waitUntilReady(element(by.css('.nick-choose button')))
+    element(by.model('chat.nick.name')).sendKeys('myNick')
+    element(by.css('.nick-choose button')).click()
+
+    waitUntilReady(element(by.model('chat.text')))
+    waitUntilReady(element(by.css('.panel-footer button')))
+    waitUntilReady(element(by.css('.panel-heading button')))
+
+    element(by.model('chat.text')).sendKeys('testing text')
+    element(by.css('.panel-footer button')).click()
+    element(by.css('.panel-heading button')).click()
+  })
+
   it('should have disabled Connect Button if text input is empty', function () {
     waitUntilReady(element(by.css('.nick-choose button')))
     expect(element(by.css('.nick-choose button')).isEnabled()).toBe(false)
+  })
+
+  it('should have disabled Send Button if text input is empty', function () {
+    waitUntilReady(element(by.model('chat.nick.name')))
+    waitUntilReady(element(by.css('.nick-choose button')))
+
+    element(by.model('chat.nick.name')).sendKeys('myNick')
+    element(by.css('.nick-choose button')).click()
+
+    waitUntilReady(element(by.css('.panel-footer button')))
+    expect(element(by.css('.panel-footer button')).isEnabled()).toBe(false)
+  })
+
+  it('should connect with a nickname and send a message with ENTER', function () {
+    waitUntilReady(element(by.model('chat.nick.name')))
+    waitUntilReady(element(by.css('.nick-choose button')))
+    element(by.model('chat.nick.name')).sendKeys('myNick')
+    element(by.css('.nick-choose button')).sendKeys(protractor.Key.ENTER)
+
+    waitUntilReady(element(by.model('chat.text')))
+    waitUntilReady(element(by.css('.panel-footer button')))
+    waitUntilReady(element(by.css('.panel-heading button')))
+
+    element(by.model('chat.text')).sendKeys('testing text')
+    element(by.css('.panel-footer button')).sendKeys(protractor.Key.ENTER)
+    element(by.css('.panel-heading button')).click()
   })
 })
