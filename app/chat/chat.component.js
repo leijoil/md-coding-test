523f7ae545aa75c7e710cfc8729a15ea1571d012
Ilja Leijo <ilja.leijo@gmail.com>
2016-06-07 23:43:03 +0300

    chat component works with view and service

--- app/chat/chat.component.js
+++ app/chat/chat.component.js
@@ -8,7 +8,12 @@ angular.module('mdCodingTestApp')
   .controller('ChatCtrl', function ($scope, ChatService) {
     var chat = this
     chat.connected = false
+    chat.date = new Date()
+    chat.error = ''
+    chat.messageObjects = []
     chat.nick = {name: null}
+    chat.text = ''
+    chat.welcomeShowed = false
 
     chat.connect = function () {
       ChatService.connect()
@@ -19,13 +24,62 @@ angular.module('mdCodingTestApp')
         console.log(error)
         chat.error = 'WebSocket error'
       } else {
-        console.log(error, readyState, message)
+        chat.publishMessage(readyState, message)
       }
     })
+
+    chat.publishMessage = function (readyState, message) {
+      try {
+        if (readyState === 1) {
+          chat.connected = true
+        }
+        if (readyState === 3) {
+          chat.reset()
+          $scope.$apply()
+        }
+        if (message !== null) {
+          if (!chat.welcomeShowed) {
+            message = chat.welcomeByNick(message)
+            chat.welcomeShowed = true
+          }
+          message = JSON.parse(message)
+          chat.messageObjects.push(message)
+          $scope.$apply()
+        }
+      } catch (error) {
+        console.log(error)
+        chat.error = 'Whoops, something went wrong'
+        $scope.$apply()
+      }
+    }
+
+    chat.send = function () {
+      var messageObj = {
+        sender: this.nick.name,
+        message: chat.text
+      }
+      chat.messageObjects.push(messageObj)
+      ChatService.send(chat.text)
+      chat.text = ''
+    }
+
+    chat.disconnect = function () {
+      ChatService.close()
+    }
+
     chat.welcomeByNick = function (welcomeMessage) {
       if (welcomeMessage.indexOf('Anonymous') >= 0) {
         welcomeMessage = welcomeMessage.replace('Anonymous', chat.nick.name)
       }
       return welcomeMessage
+    }
+
+    chat.reset = function () {
+      chat.messageObjects = []
+      chat.connected = false
+      chat.nick = {name: null}
+      chat.welcomeShowed = false
+      chat.text = ''
+      chat.error = ''
     }
   })
