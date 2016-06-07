7942e19d03a156f658e9dc956943964c884dff19
Ilja Leijo <ilja.leijo@gmail.com>
2016-06-07 22:23:56 +0300

    test for disabled button

--- test/e2e/chat.spec.js
+++ test/e2e/chat.spec.js
@@ -1,6 +1,23 @@
-describe('', function() {
-  it('should add a todo', function() {
-    browser.get('http://localhost:9000');
+describe('directive tests', function() {
 
-  });
+  var waitUntilReady = function (elm) {
+    browser.wait(function () {
+      return elm.isPresent()
+    },10000)
+    browser.wait(function () {
+      return elm.isDisplayed()
+    },10000)
+  }
+  
+  beforeEach(function() {
+    browser.get('http://localhost:9000')
+    browser.manage().window().maximize()
+    browser.waitForAngular()
+  })
+  
+  it('should have disabled Connect Button if text input is empty', function() {
+    waitUntilReady(element(by.css('.nick-choose button')));
+    expect(element(by.css('.nick-choose button')).isEnabled()).toBe(false);
+  })
+
 });
